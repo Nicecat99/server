aff75696225aaefd696314158c9421270c9bbf7f
Nicecat99 <94713118+Nicecat99@users.noreply.github.com>
2024-03-31 17:51:48 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const port = 5000; //localhost:5000
+const port = 5000; // localhost:5000
+const serverPort = 1000; //WebSocket server: "wss://localhost:1000" NOT https/http
+const Server = require('ws');
+const wss = new WebSocket.Server({port:serverPort});
 const http = require('http');
 function server(req,res){
-	res.write('Success');
+	res.write('Websocket server is: wss://localhost:1000');
 	res.end();
 }
 
